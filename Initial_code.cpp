ed54b7bcf97005b5fc4eb52ff3f38d8c348f03ce
Sahil0912 <sahiljakhar.09122005@gmail.com>
2023-12-12 23:27:51 +0530

    added new file for initial code

---
+++ Initial_code.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+#define ll long long
+#define mod 1000000007
+#define F first
+#define S second
+#define pb push_back
+#define ep emplace_back
+#define mp make_pair
+
+#define rep(i,a,b) for (int i = a; i < b; i++)
+
+void solve() {
+
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		solve();
+	}
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # CP-templates
-These are cp templates I use while giving contests or doing cp problems
+These are cp templates I use while giving contests or doing cp problems (cf, codechef, atcoder)
