c63237c99bf337b3b9d72b9b50d7ad8bcacacf21
Sahil0912 <sahiljakhar.09122005@gmail.com>
2024-01-11 11:21:50 +0530

    Latest snippet

--- Initial_code.cpp
+++ Initial_code.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 #define endl '\n'
 #define ll long long
+#define int long long
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef vector<ll> vll;
@@ -10,19 +11,24 @@ typedef pair<ll, ll> pll;
 #define pb push_back
 #define eb emplace_back
 #define mp make_pair
+#define srt(v) sort(v.begin(),v.end())
+#define yes() cout<<"YES"<<endl
+#define no() cout<<"NO"<<endl
 const ll mod = 1e9 + 7;
+const ll N = 1e6;
 
 ll gcd(ll a, ll b) {if (b == 0) return a; return gcd(b, a % b);}
 
-int main()
+int32_t main()
 {
 	ios::sync_with_stdio(false); cin.tie(NULL);
 #ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
 #endif
 
 	int tt; cin >> tt;
+	//tt = 1;
 	while (tt--) {
 
 	}
